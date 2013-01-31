126165927dbac0236f7587662f6d609610ef6ac7
Bruce Yinhe <haoqing.yinhe@gmail.com>
2013-01-31 18:40:38 +0100

    fixed sidebar. added functions.php and content.php

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php 
+
+/*
+ * load the post content div
+ */
+function get_content() {
+	require_once "content.php";
+}
+?>
--- index.php
+++ index.php
@@ -1,37 +1,10 @@
-<?php /*?><?php require_once(header.php); ?>
-<?php require_once(footer.php); ?>
-<?php require_once(sidebar.php); ?><?php */?>
-
-
+<?php require_once "functions.php"; ?>
 <?php get_header(); ?>
 
 <div id="main">
   <?php get_sidebar(); ?>
-
-  <div id="content">
-    <h1>Main Content Area</h1>
-    <?php if (have_posts()) : 
-	while (have_posts()) : the_post(); ?>
-    <h1>
-      <?php the_title(); ?>
-    </h1>
-    <h4>Posted on
-      <?php the_time('F jS, Y') ?>
-    </h4>
-    <p>
-      <?php the_content(__('(more...)')); ?>
-    </p>
-    <hr>
-    <?php endwhile; 
-    else: ?>
-    <p>
-      <?php _e("Sorry, we couldn't find the post you are looking for."); ?>
-    </p>
-    <?php endif; ?>
-  </div>
+  <?php get_content(); ?>
 </div>
 <div id="delimiter"></div>
 <?php get_footer(); ?>
-
-</body>
-</html>
+</body></html>
